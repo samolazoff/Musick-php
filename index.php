01e76a2727311a70779a49a346b3f1f999497561
Samolazoff <samolazov.roman.alex@gmail.com>
2024-08-07 11:25:41 +0300

    ADD header's app

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     ?>
 <body>
     <?php 
+        require_once 'templates/header.php';
         require_once 'templates/footer.php';
     ?>
 </body>
---
+++ templates/header.php
@@ -0,0 +1,5 @@
+<header class="app-header container-fluid">
+   <?php
+    require_once "navbar.php";
+   ?>
+</header>
